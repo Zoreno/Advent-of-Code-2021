48a34ed0760d57586551ae2e2ab46b9095eca9a7
Joakim Bertils <metallica317@spray.se>
2021-12-19 13:56:38 +0100

    Day 2

---
+++ day02/02.cpp
@@ -0,0 +1,60 @@
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+std::vector<std::pair<std::string, int>> ReadInstructionsFromFile(const char *path)
+{
+  std::ifstream input(path);
+
+  if (!input.is_open()) {
+    std::cout << "Could not open file " << path << std::endl;
+    return {};
+  }
+
+  std::string line;
+  std::vector<std::pair<std::string, int>> result;
+
+  while (std::getline(input, line)) {
+    std::istringstream iss(line);
+    std::string instruction;
+    int value;
+    iss >> instruction >> value;
+    result.push_back({instruction, value});
+  }
+
+  return result;
+}
+
+int main(int argc, const char **argv)
+{
+  if (argc != 2) {
+    std::cout << "Usage: " << argv[0] << " <data file>" << std::endl;
+    return 1;
+  }
+
+  auto instructions = ReadInstructionsFromFile(argv[1]);
+  int hpos = 0;
+  int depth = 0;
+  int aim = 0;
+
+  for (auto &[instruction, value] : instructions) {
+    if (instruction == "forward") {
+      hpos += value;
+      depth += aim * value;
+    }
+
+    if (instruction == "down") {
+      aim += value;
+    }
+
+    if (instruction == "up") {
+      aim -= value;
+    }
+  }
+
+  std::cout << "Final position: " << hpos << "," << depth << std::endl;
+  std::cout << "Product: " << hpos * depth << std::endl;
+  std::cout << "Aim: " << aim << std::endl;
+}
